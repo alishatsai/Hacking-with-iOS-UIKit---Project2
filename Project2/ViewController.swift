2fca5b2b81573e5a8a19d24baca2da421e99f929
alishatsai <aliciatsai919@gmail.com>
2022-09-20 07:36:35 +0800

    Shuffle countries array and make variable correctAnswer.
    Shuffle countries before set up image.
    Make correct answer  variable to track which one is correct.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -16,12 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","uk","us"]
+        correctAnswer = Int.random(in: 0...2)
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -33,6 +35,11 @@ class ViewController: UIViewController {
         askQuestion()
     }
     func askQuestion() {
+        // shuffle before setImage
+        countries.shuffle()
+        
+        title = countries[correctAnswer].uppercased()
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
