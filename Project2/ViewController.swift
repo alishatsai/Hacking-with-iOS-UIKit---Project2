94524f1b4a1870db0f972464e48aa1e7cad91bb6
alishatsai <aliciatsai919@gmail.com>
2022-09-19 08:08:21 +0800

    Place askQuestion method in ViewDidLoad and set up buttons' borderwidth and color.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -22,6 +22,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia","france","germany","ireland","italy","monaco","nigeria","poland","russia","uk","us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
     }
     func askQuestion() {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
