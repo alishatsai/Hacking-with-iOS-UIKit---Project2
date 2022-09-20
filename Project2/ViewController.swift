66331189cbb26c80784843c8011f2422ee2f17a1
alishatsai <aliciatsai919@gmail.com>
2022-09-20 23:47:27 +0800

    Set up IBAction method buttonTapped.
    1.Check if sender's tag equals to correctAnswer.
    2.Add alert controller.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -45,6 +45,19 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
     
 }
